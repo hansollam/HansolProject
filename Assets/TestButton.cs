1c0dce781daea03ad5e191de0c7ba80fff55380a
hansollam <hansollam0809@gmail.com>
2016-09-14 02:09:04 +0800

    test

---
+++ Assets/TestButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButton : MonoBehaviour {
+
+    public GameObject Sphere;
+    public Rigidbody rb;
+    public float thrust = 5.0f;
+    void Awake()
+    {
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+   public void OnClick()
+    {
+        rb.AddForce(transform.forward * thrust);
+    }
+}
